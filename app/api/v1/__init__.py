0de25c2bc292f18002109fc4f6f2f0c3aa15ddc6
bafiam <stevebafiam@gmail.com>
2018-11-29 18:30:59 +0300

    [finished #16233528] create user account and user login

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,4 +1,5 @@
 from .views_records import RedFlagRecords, RedFlagRecord
+from .views_users import CreateUser, GetUserLogin
 from flask_restful import Resource, Api
 from flask import Blueprint
 
@@ -6,3 +7,6 @@ version_1 = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 api = Api(version_1)
 api.add_resource(RedFlagRecords, '/red_flag_records' )
 api.add_resource(RedFlagRecord, '/red_flag_record/<int:id>' )
+api.add_resource(CreateUser, '/register')
+api.add_resource(GetUserLogin, '/login')
+
---
+++ app/api/v1/models_users.py
@@ -0,0 +1,47 @@
+user_model = []
+
+
+class UserModel():
+    def __init__(self):
+        self.db = user_model
+
+    def data_save_user(self, firstname, lastname, othername, email, phoneNumber, username, password):
+        is_valid = self.validate_the_user_username()
+        if is_valid:
+            return {"message":"The user does exist"}
+        else:
+            user_account_data = {
+                "id": self.__user__id(),
+                "firstname": firstname,
+                "lastname": lastname,
+                "othername": othername,
+                "email": email,
+                "phoneNumber": phoneNumber,
+                "username": username,
+                "password": password,
+
+        }
+        self.db.append(user_account_data)
+        return self.db
+
+    # generate user ids
+
+    def __user__id(self):
+        if len(self.db):
+            return self.db[-1]["id"] + 1
+        else:
+            return 1
+
+    def validate_the_user_username(self):
+        for username in self.db:
+            if username['username'] == username:
+                return True
+            else:
+                return False
+
+    def get_user_data_login(self, username):
+        for user in self.db:
+            if user['username'] == username:
+                return user
+
+
