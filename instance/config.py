1b380f2c131589262e29005f57b20e4e43f3cf4d
bafiam <stevebafiam@gmail.com>
2018-11-30 08:46:21 +0300

    [chore] set up the enviroment

---
+++ instance/config.py
@@ -0,0 +1,27 @@
+import os
+
+class Config(object):
+    """
+        Parent configuration class.
+    """
+    DEBUG = False
+    DATABASE_URL = os.getenv('DATABASE_URL')
+class DevelopmentConfig(Config):
+    """
+        Configurations for Development.
+    """
+    DEBUG = True
+    os.environ['ENV'] = 'development'
+
+class TestingConfig(Config):
+    """
+        Configurations for Testing
+    """
+    TESTING = True
+    DEBUG = True
+    os.environ['ENV'] = 'testing'
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig
+}
