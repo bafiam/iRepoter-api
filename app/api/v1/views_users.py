7d5d43c981707a01b484ff1bd308d34dafdd72c0
bafiam <stevebafiam@gmail.com>
2018-12-07 12:16:03 +0300

    [finished #162498826] add complex password validation and set token time out

--- app/api/v1/views_users.py
+++ app/api/v1/views_users.py
@@ -8,6 +8,7 @@ from .models import UserModel
 
 class CreateUser(Resource, UserModel):
     """user account creation"""
+
     def __init__(self):
         self.db = UserModel()
         self.reqparse = reqparse.RequestParser()
@@ -20,14 +21,21 @@ class CreateUser(Resource, UserModel):
         if not user_data['username'] or not user_data['password']:
             return make_response(jsonify({'error': 'Username and password required'}), 400)
         elif not str.isalpha(user_data['username']):
-            return make_response(jsonify({'error': 'All characters in the Username string can only contain alphabets'}), 400)
+            return make_response(jsonify({'error': 'All characters in the Username string can only contain alphabets'}),
+                                 400)
         elif not re.match(r"(^[a-zA-z0-9_.]+@[a-zA-z0-9-]+\.[a-z]+$)", user_data['email']):
             return make_response(jsonify({'error': 'Provide a valid email address'}), 400)
         else:
             if len(user_data['password']) < 7:
-                return make_response(jsonify({'error':
-                             'Password must be atleast 8 characters long!'
-                         }), 400)
+                return make_response(jsonify({'error': 'Password must be at least 8 characters long!'}), 400)
+            elif re.search('[0-9]', (user_data['password'])) is None:
+                return make_response(jsonify({'error': 'Password must has a number in it!'}), 400)
+            elif re.search('[A-Z]', (user_data['password'])) is None:
+                return make_response(jsonify({'error': 'Password must has a capital letter in it!'}), 400)
+            elif re.search('[a-z]', (user_data['password'])) is None:
+                return make_response(jsonify({'error': 'Password must has a alphabet letter in it!'}), 400)
+            elif re.search('[!,#,$,%,&,*,+,-,/,<,=,>,?,@,\,^,_,{,|,},~,]', (user_data['password'])) is None:
+                return make_response(jsonify({'error': 'Password must has a special character in it!'}), 400)
             else:
                 create_account = user_data
                 email = create_account['email']
@@ -42,9 +50,9 @@ class CreateUser(Resource, UserModel):
 
                     return make_response(jsonify({
                         "message": "Registration successfully",
-                        "access token":access_token,
+                        "access token": access_token,
 
-                        "User information":[
+                        "User information": [
                             username,
                             email,
                         ]
@@ -53,6 +61,7 @@ class CreateUser(Resource, UserModel):
 
 class GetUserLogin(Resource, UserModel):
     """user login and validation"""
+
     def __init__(self):
         self.db = UserModel()
         self.reqparse = reqparse.RequestParser()
@@ -72,9 +81,9 @@ class GetUserLogin(Resource, UserModel):
                 for user in self.db.get_all_users():
                     if user['username'] == username:
                         if bcrypt.checkpw(password.encode('utf-8'), user['password'].encode('utf-8')):
-                            access_token = create_access_token(identity=username)
+                            access_token = create_access_token(identity=username, expires_delta=None)
                             return make_response(jsonify({"message": "Login successfully",
-                                                          "access_token":access_token
+                                                          "access_token": access_token
                                                           }), 200)
                         else:
                             return make_response(jsonify({"message": "Wrong password or username"}), 400)
