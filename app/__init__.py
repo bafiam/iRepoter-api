5a5689e2186a1ad1d6bec417389f57d0e139fe5e
bafiam <stevebafiam@gmail.com>
2018-12-08 21:02:42 +0300

    [finished #162525559] setting up db connection and table creation

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,19 @@
-from flask import Flask, Blueprint
+from flask import Flask
 from flask_jwt_extended import JWTManager
 from instance.config import app_config
 from .api.v1 import version_1 as v1
-
+from database import create_tables
 import datetime
-time_out_token = datetime.timedelta(6000)
+time_out_token = datetime.timedelta(seconds=6000)
+
+
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config['development'])
-    app.config['SECRET_KEY'] = 'some636363gddd7e7@#$5ffff^^^^&hhshdhdhdfhfdhdg'
+    app.config['SECRET_KEY'] = 'some636363hhhhhgddd7e7@#$5ffff^^^^&hhshdhdhdfhfdhdg'
     app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = time_out_token
     JWTManager(app)
+    create_tables()
     app.register_blueprint(v1)
     return app
--- database/__init__.py
+++ database/__init__.py
@@ -1,26 +1,27 @@
 import psycopg2
-import os
 from .database_config import DatabaseTables
 
 
 def db_conn():
     """Create Database Connection"""
-    db_url="'ireporter', user='bafiam_admin', password='bafiam', host='localhost', port='5432'"
-
-    connection = psycopg2.connect(db_url)
+    connection = psycopg2.connect(database="ireporter", user="bafiam_admin", password="bafiam", host="127.0.0.1",
+                                  port="5432")
+    print("Opened database successfully")
     return connection
 
 
 def create_tables():
     """Creates the Tables"""
 
+    # connect the db
     connection = db_conn()
-    cursor = connection.cursor()
-
+    # create a  cursor that will be used though out the program
+    cur = connection.cursor()
+    # create the database tables after the db has been created
     database = DatabaseTables()
     queries = database.table_query()
-
+    # take a table from the tables def and depending on the one execute it and close the connection when do
     for sql in queries:
-        cursor.execute(sql)
+        cur.execute(sql)
         connection.commit()
-    cursor.close()
+    cur.close()
